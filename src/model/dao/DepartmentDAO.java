37f10b58177e0d61802108946bdf6589c94a0080
ed-cavalcanti <edney2809@gmail.com>
2024-01-06 21:16:17 -0300

    [fix] DepartmentDao

--- src/model/dao/DepartmentDAO.java
+++ src/model/dao/DepartmentDAO.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 import model.entities.Department;
 
-public interface DepartmentDAO {
-
+public interface DepartmentDao {
     void insert(Department obj);
 
     void update(Department obj);
